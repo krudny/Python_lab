4d51e6df1e2fb5286a6b1c1cb275e68383c80bcd
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-02-29 13:43:19 +0100

    finished exercise 1

---
+++ Lab0/zad1.py
@@ -0,0 +1,32 @@
+import string
+
+variables = string.ascii_lowercase
+operators = '|&>'
+
+
+def check(expression):
+    state = True
+    bracket_count = 0
+
+    for char in expression:
+        if state:
+            if char == '(':
+                bracket_count += 1
+            elif char in variables:
+                state = False
+            else:
+                return False
+        else:
+            if char == ')':
+                bracket_count -= 1
+            elif char in operators:
+                state = True
+            else:
+                return False
+
+        if bracket_count < 0:
+            return False
+
+    return bracket_count == 0 and not state
+
+
